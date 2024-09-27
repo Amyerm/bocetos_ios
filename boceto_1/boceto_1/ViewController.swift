5baffecc240732a00002314869d7cc1bee3d177c
alumno <alumno@pc-candelap.uacj.mx>
2024-09-27 10:48:38 -0600

    [Agregar]generador citas aleatorias

--- boceto_1/boceto_1/ViewController.swift
+++ boceto_1/boceto_1/ViewController.swift
@@ -11,6 +11,9 @@ class ViewController: UIViewController {
     
     var cita_para_enviar: Cita = Cita(quien_lo_dijo: "Creeper", que_dijo: "Tssseñor")
     var citas_disponibles: GeneradorDeCitas = GeneradorDeCitas()
+    var numero_aleatorio: Int = Int.random(in: 0...25)
+        
+    @IBOutlet weak var labelcito: UILabel!
     
     override func viewDidLoad() {
         citas_disponibles.generar_citas_falsas()
@@ -18,12 +21,46 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let valor = Int.random(in: 0...100)
+        actualizar_cantidad()
     }
+    
+    func actualizar_cantidad(){
+        labelcito.text = String(citas_disponibles.citas_creadas.count)
+        }
 
     @IBSegueAction func al_abrir_pantalla_citas(_ coder: NSCoder) -> ControladorPantallaCitas? {
         return ControladorPantallaCitas(cita_para_citar: citas_disponibles.obtener_cita_aleatoria(), coder: coder)
     }
     
+    @IBAction func volver_a_pantalla_inicio(segue: UIStoryboardSegue)
+    {
+        if let pantalla_cita = segue.source as? ControladorPantallaCitas{
+            citas_disponibles.agregar_cita(pantalla_citas.cita_actual.que_dijo, quien_lo_dijo: pantalla_citas.cita_actual.quien_lo_dijo)
+        }
+        // print(pantalla_citas?.cita_actual.texto)
+                
+                /*
+                if let pantalla_citas = segue.source as? ControladorPantallaCitas {
+                    citas_disponibles.agregar_cita(pantalla_citas.cita_actual.texto,
+                                                   quien_lo_dijo: pantalla_citas.cita_actual.nombre)
+                }
+                else{
+                    print("Eso no era un objeto de tipo <ControladorPantallaCitas>")
+                }
+                 */
+                
+                
+                actualizar_cantidad()
+                
+            }
+            
+            /*@IBAction func regresar(segue: UIStoryboardSegue){
+                print("En esto del proceso")
+                print(segue.source as? ControladorPantallaCitas)
+                let elemento = segue.source as? ControladorPantallaCitas
+                print(elemento?.cita_actual.nombre)
+            }*/
+    
+
 }
 
--- boceto_1/boceto_1/controladores_de_vistas/ControladorPantallaCitas.swift
+++ boceto_1/boceto_1/controladores_de_vistas/ControladorPantallaCitas.swift
@@ -14,29 +14,28 @@ class ControladorPantallaCitas: UIViewController {
     @IBOutlet weak var que_dijo_muro_texto: UILabel!
     
     var cita_actual: Cita
-    
-    required init?(coder: NSCoder) {
         
-        self.cita_actual = Cita(quien_lo_dijo: "Desarrollador", que_dijo: "Tenemos un problema. Reportalo por una rebanada de pastel")
-        super.init(coder: coder)
-        print("Error: Se ha cargado el default de INIT")
-    }
-    
-    init?(cita_para_citar: Cita, coder: NSCoder){
-        self.cita_actual = cita_para_citar
-        super.init(coder: coder)
-    }
-    
-    override func viewDidLoad() {
-            super.viewDidLoad()
+        required init?(coder: NSCoder) {
         
-        inicializar_pantalla()
-
+            self.cita_actual = Cita(quien_lo_dijo: "Desarrollador", que_dijo: "Tenemos un problema. Reportalo por una rebanada de pastel")
+            super.init(coder: coder)
+            print("Error: Se ha cargado el default de INIT")
         }
-    
-    func inicializar_pantalla(){
-        nombre_de_quien_lo_dijo.text = cita_actual.nombre
-        que_dijo_muro_texto.text = cita_actual.texto
-    }
+        
+        init?(cita_para_citar: Cita, coder: NSCoder){
+            self.cita_actual = cita_para_citar
+            super.init(coder: coder)
+        }
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            inicializar_pantalla()
+        }
+        
+        func inicializar_pantalla(){
+            nombre_de_quien_lo_dijo.text = cita_actual.nombre
+            que_dijo_muro_texto.text = cita_actual.texto
+        }
 
 }
